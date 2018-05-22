64176dcc51cd4d0d8aed55b0ed8cb8caafa8e8ec
KRXPS\krer <kinseyr1313@gmail.com>
2018-05-23 08:53:53 +1000

    cbs working properly now

--- cbs1.py
+++ cbs1.py
@@ -4,14 +4,16 @@ import math
 import da
 from operator import itemgetter
 
+
 """
 Implementation of task 2 in Computational Genomics assignment 2
-Kinsey Reeves 18/05/2018
+Kinsey Reeves 18/05/2018. 
 695705
 """
 
+
 final_segs = []
-Z_THRESH = 10
+Z_THRESH = 1
 
 def print_segs(segs):
     
@@ -27,9 +29,6 @@ def print_seg(segs):
     for i in segs:
         print(i)
 
-
-def get_starts_ends(segment):
-    return 
 
 def setup(filename):
     
@@ -50,34 +49,50 @@ def setup(filename):
         data[i][3] = lr
     return data
 
+
+def precompute_sums(data):
+    sums = []
+    sum_i = 0
+    
+    for bi in data:
+        #print(bi[3])
+        sum_i+=bi[3]
+        sums.append(sum_i)
+    
+    #print(sums)
+    return sums
+
 def cbs(data):
     sum_i = 0
     found = False
     best = -1*sys.maxsize
     best_i = 0
     best_j = 0
+    sums = precompute_sums(data)
 
     n = len(data)
     sum_n = sum((x[3] for x in data))
 
     for i in range(0, n-1):
-        sum_i += data[idx(i,n)][3]
+        sum_i = sums[i]
         sum_j = 0
 
-        for j in range(i+1, i+n):
+        for j in range(i+1, n):
             
-            sum_j+=data[idx(j,n)][3]
-            z = ( (1.0/(j-i)) + (1.0/(n-j+i)) )**(-1.0/2)
+            sum_j = sums[j]
+            z = ( (1.0/(j-i)) + (1.0/(n-j+i)) )**(-0.5)
             z*= ( ((sum_j-sum_i)/(j-i)) - ((sum_n-sum_j+sum_i)/(n-j+i)))   
             z = abs(z)
         
             if(z>=Z_THRESH):
+
                 found = True
                 if(z>best):
                     best = z
                     best_i = i
                     best_j = j
-                
+
+    #exit(0)            
     if(found):
         segs = splice(best_i, best_j, data)
         cbs(segs[0])
@@ -103,31 +118,30 @@ def splice(i, j, data):
     segments
     """
     n = len(data)
-    out = ()
     splice_a = []
     splice_b = []
-    i = idx(i, n)
-    j = idx(j, n)
     i+=1
     j+=1
-    if(i < j):
-        splice_a = data[i:j]
-        splice_b = data[j:] + data[0:i]
-    else:
-        splice_a = data[i:] + data[0:j]
-        splice_b = data[j:i]
+    splice_a = data[i:j]
+    splice_b = data[j:] + data[0:i]
+    print(data)
+    exit(0)
+    
 
     return (splice_a, splice_b)
     
 
-data = setup("tumor.txt")
+data = setup("sample.txt")
+
 a = cbs(data)
+
+
 out = []
 for segment in final_segs:
 
     avg = sum([x[3] for x in segment])/len(segment)
-    #if(abs(avg)>0.1):
-    out.append(segment[0][0:2] + [segment[-1][2]] + [avg] + [len(segment)])
+    if(abs(avg)>0.1):
+        out.append(segment[0][0:2] + [segment[-1][2]] + [avg] + [len(segment)])
     
     for line in segment:
         print(line)
